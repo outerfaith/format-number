7a07fb6f56f1a46334ab24df013f385880b2e637
outerfaith <183041933+outerfaith@users.noreply.github.com>
2025-11-10 21:24:45 -0500

    feat: project structure changes

--- src/_internal/init.lua
+++
@@ -1,8 +0,0 @@
-return table.freeze({
-	class = require(script.class),
-	enums = require(script.enums),
-	format = require(script.format),
-	formatter_settings = require(script.formatter_settings),
-	decimal_conversion = require(script.decimal_conversion),
-	skeleton = require(script.skeleton),
-})
